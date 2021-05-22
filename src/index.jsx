5d9b967b083563373a4d22517aa8ed7380e8c97e
Ivana Klementová <klementova.vf@gmail.com>
2021-05-22 15:58:42 +0200

    14-15

---
+++ src/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './style.css';
+import Joke from './Joke/index';
+import { jokes } from './joke';
+
+const App = () => {
+  return (
+    <>
+      <div class="container">
+        {jokes.map((joke) => (
+          <Joke
+            key={joke.id}
+            userAvatar={joke.avatar}
+            userName={joke.name}
+            text={joke.text}
+            likes={joke.likes}
+            dislikes={joke.dislikes}
+          ></Joke>
+        ))}
+      </div>
+    </>
+  );
+};
+
+render(<App />, document.querySelector('#app'));
